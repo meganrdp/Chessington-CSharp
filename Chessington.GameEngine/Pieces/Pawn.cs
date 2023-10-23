9568844751c92e4c20b6b5a23d0ccb802a64dc08
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-23 16:18:04 +0100

    Refactored after Red2

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -12,49 +12,29 @@ public class Pawn : Piece
 
     public override IEnumerable<Square> GetAvailableMoves(Board board)
     {
+        var result = new List<Square>();
         var currentSquare = board.FindPiece(this);
+        
+        if (Player == Player.White)
+        {
+            result.Add(Square.At(currentSquare.Row - 1, currentSquare.Col));
+        }
+        else 
+        {
+            result.Add(Square.At(currentSquare.Row + 1, currentSquare.Col));
+        }
         
         if (Player == Player.White && currentSquare.Row == 6)
         {
-            var result = new List<Square>
-            {
-                Square.At(currentSquare.Row - 2, currentSquare.Col),
-                Square.At(currentSquare.Row - 1, currentSquare.Col)
-
-            };
-
-            return result;
+            result.Add(Square.At(currentSquare.Row - 2, currentSquare.Col));
         }
-        else if (Player == Player.Black && currentSquare.Row == 1)
+
+        if (Player == Player.Black && currentSquare.Row == 1)
         {
-            var result = new List<Square>
-            {
-                Square.At(currentSquare.Row + 2, currentSquare.Col),
-                Square.At(currentSquare.Row + 1, currentSquare.Col)
-
-            };
-
-            return result;
+            result.Add(Square.At(currentSquare.Row + 2, currentSquare.Col));
         }
-        
-        else if (Player == Player.White && currentSquare.Row != 6)
-        {
-            var result = new List<Square>
-            {
-                Square.At(currentSquare.Row - 1, currentSquare.Col)
-            };
 
-            return result;
-        }
-        else
-        {
-            var result = new List<Square>
-            {
-                Square.At(currentSquare.Row + 1, currentSquare.Col)
-            };
-
-            return result;
-        }       
+        return result;       
       
     }
 }
