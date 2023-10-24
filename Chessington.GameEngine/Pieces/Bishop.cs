0fb5724acdff5e5ee4b321fb375233949c00ac16
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-24 12:22:11 +0100

    Red-4 passed, Bishop able to move diagonally in all directions; also refactored Rook code to make it easier to use elsewhere.

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -12,6 +12,33 @@ public class Bishop : Piece
 
     public override IEnumerable<Square> GetAvailableMoves(Board board)
     {
-        return Enumerable.Empty<Square>();
+        var result = new List<Square>();
+        var currentSquare = board.FindPiece(this);
+
+        // Define direction for movement
+        var directions = new List<Square>
+        {
+            Square.At(1, 1),
+            Square.At(1, -1),
+            Square.At(-1, 1),
+            Square.At(-1, -1),
+        };
+
+        // Loop through each direction
+        foreach (var direction in directions)
+        {
+            for (int i = 1; i < 8; i++)
+            {
+                var square = Square.At(currentSquare.Row + i * direction.Row, currentSquare.Col + i * direction.Col);
+                if (square.Row > 7 || square.Row < 0 || square.Col > 7 || square.Col <0)
+                {
+                    // stop because square is not on board
+                    break;
+                }
+                result.Add(square);
+            }
+        }
+
+        return result;
     }
 }
--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -16,18 +16,26 @@ public class Rook : Piece
         var currentSquare = board.FindPiece(this);
 
         // Define direction for movement
-        var directions = new int[] { -1, 1 };
+        var directions = new List<Square>
+        {
+            Square.At(1, 0),
+            Square.At(0, 1),
+            Square.At(-1, 0),
+            Square.At(0, -1),
+        };
 
         // Loop through each direction
-        foreach (var rowDirection in directions)
+        foreach (var direction in directions)
         {
-            for (int i = 1; i <= 7; i++)
+            for (int i = 1; i < 8; i++)
             {
-                // Move vertically
-                result.Add(Square.At(currentSquare.Row + i * rowDirection, currentSquare.Col));
-
-                // Move horizontally
-                result.Add(Square.At(currentSquare.Row, currentSquare.Col + i * rowDirection));
+                var square = Square.At(currentSquare.Row + i * direction.Row, currentSquare.Col + i * direction.Col);
+                if (square.Row > 7 || square.Row < 0 || square.Col > 7 || square.Col <0)
+                {
+                    // stop because square is not on board
+                    break;
+                }
+                result.Add(square);
             }
         }
 
