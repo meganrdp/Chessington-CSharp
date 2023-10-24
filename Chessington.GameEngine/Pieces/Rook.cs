4540a5fffe228fb1a30b63298794f542a97447c9
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-24 09:27:45 +0100

    Red-3 refactored code

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -14,73 +14,23 @@ public class Rook : Piece
     {
         var result = new List<Square>();
         var currentSquare = board.FindPiece(this);
-        
-        if (Player == Player.White)
-        {
-            result.Add(Square.At(currentSquare.Row - 1, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 2, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 3, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 4, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 5, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 6, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 7, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 1, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 2, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 3, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 4, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 5, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 6, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 7, currentSquare.Col));
-            
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 1));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 2));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 3));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 4));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 5));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 6));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 7));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 1));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 2));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 3));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 4));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 5));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 6));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 7));
-            
-        }
-        else 
-        {
-            result.Add(Square.At(currentSquare.Row - 1, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 2, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 3, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 4, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 5, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 6, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row - 7, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 1, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 2, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 3, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 4, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 5, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 6, currentSquare.Col));
-            result.Add(Square.At(currentSquare.Row + 7, currentSquare.Col));
 
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 1));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 2));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 3));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 4));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 5));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 6));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col - 7));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 1));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 2));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 3));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 4));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 5));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 6));
-            result.Add(Square.At(currentSquare.Row, currentSquare.Col + 7));
+        // Define direction for movement
+        var directions = new int[] { -1, 1 };
+
+        // Loop through each direction
+        foreach (var rowDirection in directions)
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                // Move vertically
+                result.Add(Square.At(currentSquare.Row + i * rowDirection, currentSquare.Col));
+
+                // Move horizontally
+                result.Add(Square.At(currentSquare.Row, currentSquare.Col + i * rowDirection));
+            }
         }
-        
-        return result;       
+
+        return result;
     }
 }
